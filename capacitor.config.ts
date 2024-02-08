0496b62a7275d2f623f6bfa164254313b09a4d83
Ashwini Talware <81010617+ashwinitalware@users.noreply.github.com>
2024-02-08 21:53:04 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ZEP_ACADEMY',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
